4b1d2459abd806853060cad3be851784907ebc28
hoaklee <hoaklee@outlook.com>
2020-11-29 16:31:51 +0100

    initialize node

--- pcimr_localization/src/probability_location.py
+++ pcimr_localization/src/probability_location.py
@@ -1,5 +1,5 @@
-# Author LiQianhao Qianhao.li@tum.de
 #!/usr/bin/env python
+# author Li Qianhao, qianhao.li@tum.de
 
 import rospy
 import numpy as np
@@ -9,14 +9,12 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid, MapMetaData
 from visualization_msgs.msg import Marker
-from pcimr_simulation.srv import InitPos
-
+# from pcimr_simulation.srv import InitPos
 
 class probability_location:
     """
     A node
     """
-
     def __init__(self):
 
         # Initialize Subscribers
@@ -27,27 +25,46 @@ class probability_location:
         # Initialize Publisher
         self.point_pub = rospy.Publisher('/robot_pos', Point, queue_size=10)
         self.marker_pub = rospy.Publisher('/visualization/robot_pos', Marker, queue_size=10)
-        self.grid_pub = rospy.Publisher('/map_updates', OccupancyGrid, queue_size=10)
+        # self.grid_pub = rospy.Publisher('/map_update', OccupancyGrid, queue_size=10)
 
-    # callback of
+    # callback of map information
     def callback_map(self, OccupancyGrid):
         self.map_input = OccupancyGrid
 
-    # callback of
+    # callback of sensor information
     def callback_sensor(self, LaserScan):
         self.sensor_input = LaserScan
 
-    # callback of
+    # callback of move command
     def callback_move(self, String):
         self.move_input= String
 
+    # # initialize position at the first step
+    # def pos_init(self):
+
+
+    # # predict position according to previous position and move command using bayes rule
+    # def pos_predict(self):
+    #     testtest
+
+    # # update position probablity using sensor data
+    # def pos_update(self):
+    #     testtesttest
+
     def run(self, rate: float = 1):
+        # initialize position
+        # pos_init()
         while not rospy.is_shutdown():
+
+            print(self.map_input)
+            # pos_predict()
+            # pos_update()
 
             if rate:
                rospy.sleep(1/rate)
 
 if __name__ == "__main__":
+    # initialize probability_location node
     rospy.init_node('probability_location')
 
     probability_location = probability_location()
